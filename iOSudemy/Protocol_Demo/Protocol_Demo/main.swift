79d9f3d2fa0de25bc0a6d48c69ba941143bc2bf2
Kim <cestbonciel88@knou.ac.kr>
2023-09-10 20:06:24 +0900

    Add: Studying the role of protocol when it applies instance method on each different types and collections

--- iOSudemy/Protocol_Demo/Protocol_Demo/main.swift
+++ iOSudemy/Protocol_Demo/Protocol_Demo/main.swift
@@ -1,11 +1,64 @@
-//
-//  main.swift
-//  Protocol_Demo
-//
-//  Created by Seohyun Kim on 2023/09/10.
-//
+protocol CanFly {
+	func fly()
+	
+}
 
-import Foundation
+class Bird {
+	
+	var isFemale = true
+	
+	func layEgg() {
+		if isFemale {
+				print("The bird makes a new bird in a shell.")
+		}
+	}
 
-print("Hello, World!")
+}
+
+class Eagle: Bird, CanFly {
+	func fly() {
+		print("The eagle flaps its wings and lifts off into the sky.")
+	}
+	
+	func soar() {
+		print("The eagle glides in the air using air currents.")
+	}
+}
+
+
+class Penguin: Bird {
+	func swim() {
+		print("The penguin paddles through the water.")
+	}
+}
+
+struct FlyingMuseum {
+	func flyingDemo(flyingObject: CanFly) {
+		flyingObject.fly()
+	}
+}
+
+class Airplane: CanFly {
+	
+	func fly() {
+		print("The airplane uses its engine to lift off into the air.")
+	}
+}
+
+let myEagle = Eagle()
+//myEagle.fly()
+//myEagle.layEgg()
+//myEagle.soar()
+//
+let myPenguin = Penguin()
+//myPenguin.layEgg()
+//myPenguin.swim()
+//myPenguin.fly()
+
+let museum = FlyingMuseum()
+//museum.flyingDemo(flyingObject: myPenguin)
+museum.flyingDemo(flyingObject: myEagle)
+
+let myPlane = Airplane()
+museum.flyingDemo(flyingObject: myPlane)
 
