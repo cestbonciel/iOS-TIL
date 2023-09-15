8845f1ee7ea0017b3256a639a6b421f03eb04454
Kim <cestbonciel88@knou.ac.kr>
2023-09-15 22:52:32 +0900

    Add: applied  custom UIButton code to CardView

--- SwiftUI/SwiftUI.M.C/Hike/Hike/Component/CardView.swift
+++ SwiftUI/SwiftUI.M.C/Hike/Hike/Component/CardView.swift
@@ -8,6 +8,27 @@
 import SwiftUI
 
 struct CardView: View {
+	// MARK: - PROPERTIES
+	
+	@State private var imageNumber: Int = 1
+	@State private var randomNumber: Int = 1
+	
+	// MARK: - FUNCTIONS
+	
+	func randomImage() {
+		print("--- BUTTON WAS PRESSED ---")
+		print("Status: Old Image Number = \(imageNumber)")
+		repeat {
+			randomNumber = Int.random(in: 1...5)
+			print("Action: Random Number Generated = \(randomNumber)")
+		} while randomNumber == imageNumber
+					
+		imageNumber = randomNumber
+		print("Result: New Image Number = \(imageNumber)")
+		print("-- THE END ---")
+		print("\n")
+	}
+	
 	var body: some View {
 		// MARK: - CARD
 		
@@ -61,14 +82,15 @@ struct CardView: View {
 						)
 						.frame(width: 256,height: 256)
 					
-					Image("image-1")
+					Image("image-\(imageNumber)")
 						.resizable()
 						.scaledToFit()
+						.animation(.default, value: imageNumber)
 				}
 				//MARK: - FOOTER
 				Button {
 					// ACTION: Generate a random number
-					print("The button was pressed.")
+					randomImage()
 				} label: {
 					Text("Explore more")
 						.font(.title2)
@@ -80,10 +102,12 @@ struct CardView: View {
 									.customGreenMedium
 								],
 								startPoint: .top,
-								endPoint: .bottom)
+								endPoint: .bottom
+							)
 						)
-						.shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+						.shadow(color: .black.opacity(0.25),radius: 0.25, x:1, y:2)
 				}
+				.buttonStyle(GradientButton())
 				
 			}//: ZSTACK
 		} //: CARD
--- SwiftUI/SwiftUI.M.C/Hike/Hike/Utility/GradientButtonStyle.swift
+++ SwiftUI/SwiftUI.M.C/Hike/Hike/Utility/GradientButtonStyle.swift
@@ -15,12 +15,14 @@ struct GradientButton: ButtonStyle {
 			.padding(.vertical)
 			.padding(.horizontal, 30)
 			.background(
-				LinearGradient(
-					colors: [
-						.customGrayLight,
-						.customGrayMedium],
-					startPoint: .top,
-					endPoint: .bottom)
+				// Conditional Statement with Nil Coalescing
+				// Condition ? A : B
+				configuration.isPressed ?
+				// A: When User pressed the Button
+				LinearGradient(colors: [.customGrayMedium,.customGrayLight],startPoint: .top,endPoint: .bottom)
+				:
+				// B: When the Button is not pressed
+				LinearGradient(colors: [.customGrayLight,.customGrayMedium],startPoint: .top,endPoint: .bottom)
 			)
 			.cornerRadius(40)
 	}
