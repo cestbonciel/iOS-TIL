6b520b1d5fece71a62dbd87ea79ea2a2d1ea8bf4
Kim <cestbonciel88@knou.ac.kr>
2023-10-03 18:05:06 +0900

    Feat: create Info Panel View for scale, long press gesture

--- SwiftUI/SwiftUI.M.C/Pinch/Pinch/Screen/ContentView.swift
+++ SwiftUI/SwiftUI.M.C/Pinch/Pinch/Screen/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView: View {
     var body: some View {
 			NavigationView {
 				ZStack {
+					Color.clear
 					//MARK: - Page & Image
 					Image("magazine-front-cover")
 						.resizable()
@@ -70,6 +71,13 @@ struct ContentView: View {
 						isAnimating = true
 					}
 				}
+				//MARK: - INFO PANEL
+				.overlay(
+					InfoPanelView(scale: imageScale, offset: imageOffset)
+						.padding(.horizontal)
+						.padding(.top, 30)
+					,alignment: .top
+				)
 			} //: NAVIGATION
 			.navigationViewStyle(.stack)
     }
