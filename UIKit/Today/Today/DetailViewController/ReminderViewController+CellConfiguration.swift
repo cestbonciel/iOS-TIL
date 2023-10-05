c31e363098063caba4ed70c7ade65da12f231abc
Kim <cestbonciel88@knou.ac.kr>
2023-10-05 15:55:19 +0900

    Add: Create defaultConfiguration for UICollectionListCell on Todayfile

--- UIKit/Today/Today/DetailViewController/ReminderViewController+CellConfiguration.swift
+++ UIKit/Today/Today/DetailViewController/ReminderViewController+CellConfiguration.swift
@@ -5,4 +5,14 @@
 //  Created by Seohyun Kim on 2023/10/04.
 //
 
-import Foundation
+import UIKit
+
+extension ReminderViewController {
+	func defaultConfiguration(for cell: UICollectionViewListCell, at row: Row) -> UIListContentConfiguration {
+		var contentConfiguration = cell.defaultContentConfiguration()
+		contentConfiguration.text = text(for: row)
+		contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+		contentConfiguration.image = row.image
+		return contentConfiguration
+	}
+}
