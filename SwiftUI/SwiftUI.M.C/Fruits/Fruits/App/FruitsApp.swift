42ef8db73ea08a47b148da5e2b99cd2458ef76c9
Kim <cestbonciel88@knou.ac.kr>
2023-10-15 18:20:17 +0900

    Feat: making onboarding view in progress at first

---
+++ SwiftUI/SwiftUI.M.C/Fruits/Fruits/App/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Seohyun Kim on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
