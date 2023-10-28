00cedab46d787e65c12ad7a78b3da1911ffaa04a
Kim <cestbonciel88@knou.ac.kr>
2023-10-28 12:37:12 +0900

    Feat: Networking openweatherAPI

--- iOSudemy/Clima_bySeohyun/Clima/Controller/WeatherViewController.swift
+++ iOSudemy/Clima_bySeohyun/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,19 @@
 import UIKit
 
 class WeatherViewController: UIViewController, UITextFieldDelegate {
-
+	
 	@IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
+	@IBOutlet weak var temperatureLabel: UILabel!
+	@IBOutlet weak var cityLabel: UILabel!
 	@IBOutlet weak var searchTextField: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		 searchTextField.delegate = self
-    }
-
+	
+	var weatherManager = WeatherManager()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		searchTextField.delegate = self
+	}
+	
 	@IBAction func searchPressed(_ sender: UIButton) {
 		searchTextField.endEditing(true)
 		print(searchTextField.text!)
@@ -36,12 +38,15 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
 		} else {
 			textField.placeholder = "도시를 입력하세요."
 			return false
-		} 
+		}
 	}
 	
 	
 	func textFieldDidEndEditing(_ textField: UITextField) {
 		//Use searchTextField.text to get 도시 날씨 정보
+		if let city = searchTextField.text {
+			weatherManager.fetchWeather(cityName: city)
+		}
 		searchTextField.text = ""
 	}
 }
