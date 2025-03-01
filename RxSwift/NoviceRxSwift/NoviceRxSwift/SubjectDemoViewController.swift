f62b9ae4b87febca4c6e51e03ceaf5ecf6c5fcc9
Nat <cestbonciel88@knou.ac.kr>
2025-03-02 03:35:06 +0900

    Feat: codes about Subjects

--- RxSwift/NoviceRxSwift/NoviceRxSwift/SubjectDemoViewController.swift
+++ RxSwift/NoviceRxSwift/NoviceRxSwift/SubjectDemoViewController.swift
@@ -11,77 +11,81 @@ import RxCocoa
 
 class SubjectDemoViewController: UIViewController {
     
-    // SubjectDemoView를 인스턴스로 생성
-    let subjectDemoView = SubjectDemoView()
-    
-    // RxSwift DisposeBag
-    let disposeBag = DisposeBag()
-    
-    // RxSwift Subject 예시 (필요한 경우)
-    let publishSubject = PublishSubject<String>()
-    let behaviorSubject = BehaviorSubject<String>(value: "Initial")
-    let replaySubject = ReplaySubject<String>.create(bufferSize: 2)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        setupSubjectDemoView()
-        setupRxBindings()
-    }
-    
-    private func setupSubjectDemoView() {
-        // 커스텀 뷰를 컨트롤러의 view에 추가
-        subjectDemoView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(subjectDemoView)
-        NSLayoutConstraint.activate([
-            subjectDemoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            subjectDemoView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            subjectDemoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            subjectDemoView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-    }
-    
-    private func setupRxBindings() {
-        // 버튼 탭 시 RxCocoa를 통해 Subject에 이벤트 전달
-        subjectDemoView.actionButton.rx.tap.subscribe(onNext: { [weak self] in
-            guard let self = self else { return }
-            let message = "Button tapped at \(Date())"
-            print("Button action: \(message)")
-            self.publishSubject.onNext(message)
-            self.behaviorSubject.onNext(message)
-            self.replaySubject.onNext(message)
-        })
-        .disposed(by: disposeBag)
-        
-        // 스와이프 제스처 추가 및 RxCocoa를 통해 이벤트 처리
-        let swipeGesture = UISwipeGestureRecognizer()
-        swipeGesture.direction = .left
-        subjectDemoView.swipeView.addGestureRecognizer(swipeGesture)
-        
-        swipeGesture.rx.event.subscribe(onNext: { [weak self] _ in
-            guard let self = self else { return }
-            let message = "Swiped left at \(Date())"
-            print("Swipe action: \(message)")
-            self.publishSubject.onNext(message)
-            self.behaviorSubject.onNext(message)
-            self.replaySubject.onNext(message)
-        })
-        .disposed(by: disposeBag)
-        
-        // 예시로 각 Subject를 구독하여 이벤트 출력
-        publishSubject.subscribe(onNext: { value in
-            print("PublishSubject: \(value)")
-        })
-        .disposed(by: disposeBag)
-        
-        behaviorSubject.subscribe(onNext: { value in
-            print("BehaviorSubject: \(value)")
-        })
-        .disposed(by: disposeBag)
-        
-        replaySubject.subscribe(onNext: { value in
-            print("ReplaySubject: \(value)")
-        })
-        .disposed(by: disposeBag)
-    }
+       // 커스텀 뷰 인스턴스
+       let subjectDemoView = SubjectDemoView()
+       
+       // RxSwift DisposeBag
+       let disposeBag = DisposeBag()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           view.backgroundColor = .white
+           setupSubjectDemoView()
+           setupRxBindings()
+       }
+       
+       private func setupSubjectDemoView() {
+           subjectDemoView.translatesAutoresizingMaskIntoConstraints = false
+           view.addSubview(subjectDemoView)
+           NSLayoutConstraint.activate([
+               subjectDemoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+               subjectDemoView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+               subjectDemoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+               subjectDemoView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+           ])
+       }
+       
+       private func setupRxBindings() {
+           // 버튼 탭 시: 랜덤 이미지를 불러오고 UILabel1 업데이트
+           subjectDemoView.actionButton.rx.tap
+               .flatMapLatest { () -> Observable<UIImage?> in
+                   // 랜덤 시드 생성 (1~1000)
+                   let randomSeed = Int.random(in: 1...1000)
+                   let urlString = "https://picsum.photos/seed/\(randomSeed)/200/200"
+                   guard let url = URL(string: urlString) else { return Observable.just(nil) }
+                   let request = URLRequest(url: url)
+                   // URLSession을 통해 데이터 받아오기
+                   return URLSession.shared.rx.data(request: request)
+                       .map { UIImage(data: $0) }
+                       .catchAndReturn(nil)
+               }
+               .observe(on: MainScheduler.instance)
+               .subscribe(onNext: { [weak self] image in
+                   guard let self = self else { return }
+                   if let image = image {
+                       self.subjectDemoView.imageView.image = image
+                       let message = "Button tapped: Image updated"
+                       self.subjectDemoView.label1.text = message
+                       print(message)
+                   } else {
+                       self.subjectDemoView.label1.text = "Image load failed"
+                   }
+               })
+               .disposed(by: disposeBag)
+           
+           // 스와이프 제스처 추가 및 처리
+           let swipeGesture = UISwipeGestureRecognizer()
+           swipeGesture.direction = .left
+           subjectDemoView.swipeView.addGestureRecognizer(swipeGesture)
+           
+           // 랜덤 컬러 Hex 코드 배열 (중복 제거)
+           let colorHexes = [
+               "#704a9d", "#abcd8e", "#ca6e07", "#2d785a",
+               "#9fac69", "#36fb70", "#ae9af0", "#508fc6",
+               "#8b62e9", "#fc40e6", "#909759", "#8a33bd",
+               "#9aa37d", "#a26b8e", "#3af78f", "#258feb"
+           ]
+           
+           swipeGesture.rx.event.subscribe(onNext: { [weak self] _ in
+               guard let self = self else { return }
+               let randomHex = colorHexes.randomElement() ?? "#ffffff"
+               if let color = UIColor(hex: randomHex) {
+                   self.subjectDemoView.swipeView.backgroundColor = color
+               }
+               let message = "Swiped: Color updated to \(randomHex)"
+               self.subjectDemoView.label2.text = message
+               print(message)
+           })
+           .disposed(by: disposeBag)
+       }
 }
