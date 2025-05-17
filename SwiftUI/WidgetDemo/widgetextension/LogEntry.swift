3907da527f467843cef925883ba05c6ab25ce9d7
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-05-18 08:01:14 +0900

    feat:add Button on widget for interactive widget

--- SwiftUI/WidgetDemo/widgetextension/LogEntry.swift
+++ SwiftUI/WidgetDemo/widgetextension/LogEntry.swift
@@ -1,5 +1,5 @@
 //
-//  LogEntryAppIntent.swift
+//  LogEntry.swift
 //  widgetextensionExtension
 //
 //  Created by Nat Kim on 5/18/25.
@@ -7,15 +7,16 @@
 
 import Foundation
 import AppIntents
+import SwiftUI
 
 
-struct LogEntryAppIntent: AppIntent {
+struct LogEntry: AppIntent {
 	
-	static var title: LocalizedStringResource = "Log an entry to your streak."
+	static var title: LocalizedStringResource = "Log a streak."
 	
 	static var description = IntentDescription("Add 1 to your streak.")
 	
-	func perform() async throws -> some IntentResult & ReturnsValue {
+	func perform() async throws -> some IntentResult & ReturnsValue<Int> {
 		let data = DataService()
 		data.log()
 		
