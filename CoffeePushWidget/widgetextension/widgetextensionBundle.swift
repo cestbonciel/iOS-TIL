24e9cfd69b21b4d9a901a423a2d6350e0f74b7d4
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-06-24 03:41:09 +0900

    Chore: system files

---
+++ CoffeePushWidget/widgetextension/widgetextensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetextensionBundle.swift
+//  widgetextension
+//
+//  Created by Nat Kim on 6/23/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetextensionBundle: WidgetBundle {
+    var body: some Widget {
+        widgetextension()
+    }
+}
